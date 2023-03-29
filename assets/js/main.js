deb170592f3aa098f5c9819ffb0b32363eb74201
Mubin Mansuri <43163379+MubinMansuri@users.noreply.github.com>
2023-03-29 17:16:53 +0530

    working on homepage...

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,38 @@ document.addEventListener("DOMContentLoaded", function () {
     smooth: true,
   });
   
-// banner slider 
 
-
-// end here
-
-
+// logo slider
+var swiper = new Swiper(".trustedSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    414: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 50,
+    },
+  },
+});
 
 });
 // DOMContentLoaded  end
