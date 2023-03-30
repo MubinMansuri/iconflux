6c0328e71426d3fabb2d8b8ce5de326aba06d1d3
Mubin Mansuri <43163379+MubinMansuri@users.noreply.github.com>
2023-03-30 16:45:42 +0530

    footer added with Responsive...

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,41 +7,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   
 
-// logo slider
-var swiper = new Swiper(".trustedSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    414: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 6,
-      spaceBetween: 50,
-    },
-  },
-});
+  // logo slider
+  var trustedSwiper=new Swiper(".trustedSwiper",{slidesPerView:2,spaceBetween:10,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{414:{slidesPerView:2,spaceBetween:20},640:{slidesPerView:3,spaceBetween:20},768:{slidesPerView:5,spaceBetween:40},1024:{slidesPerView:6,spaceBetween:50}}});
+
+  singeSlideSwiper(".caseStudy");
 
 });
 // DOMContentLoaded  end
-
+function singeSlideSwiper(e){
+  var caseStudy = new Swiper(e, {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
 // for jQuery
 (function () {})(
 
