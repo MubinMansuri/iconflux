b03686836a2b75ffc67dac3819e4062c77f26dc9
Mubin Mansuri <43163379+MubinMansuri@users.noreply.github.com>
2023-04-07 11:41:52 +0530

    improve Scrolling text issue add btn hover effect.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add('img_loaded');
   });
   
+  window.addEventListener('scroll', () => document.querySelector('.site-header').classList.toggle('header-sticky', window.pageYOffset > 200));
+  window.addEventListener('scroll', () => document.querySelector('.site-header').classList.toggle('shadow-sm', window.pageYOffset > 200));
 
   // logo slider
   const trustedSwiper=new Swiper(".trustedSwiper",{slidesPerView:2,spaceBetween:10,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{414:{slidesPerView:2,spaceBetween:20},640:{slidesPerView:3,spaceBetween:20},768:{slidesPerView:5,spaceBetween:40},1024:{slidesPerView:6,spaceBetween:50}}});
@@ -34,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
 
     const additionalX = { val: 0 };
-    let additionalXAnim;
     let offset = 0;
     let originalImages = gsap.utils.toArray(".o-big-title .o-big-title__inner .o-big-title__txt");
     const container = document.querySelector(".o-big-title .o-big-title__inner");
@@ -43,11 +44,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // DUPLICATE IMAGES FOR LOOP
     originalImages.forEach((image) => {
       var clone = image.cloneNode(true);
+      clone.classList.add('o-big-title__txt--clone');
       container.appendChild(clone);
     });
-
     let images = gsap.utils.toArray(".o-big-title .o-big-title__inner .o-big-title__txt");
-
     // SET ANIMATION
     images.forEach((item) => {
       gsap.to(item, {
@@ -64,27 +64,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
-
-    const imagesScrollerTrigger = ScrollTrigger.create({
-      trigger: '.ico_05',
-      start: "top 100%",
-      end: "bottom",
-      markers:true,
-      onUpdate: function (self) {
-        const velocity = self.getVelocity();
-        if (velocity > 0) {
-          if (additionalXAnim) additionalXAnim.kill();
-          additionalX.val = -velocity / 4000;
-          additionalXAnim = gsap.to(additionalX, { val: 0 });
+    gsap.utils.toArray('.ico_05').forEach((section, index) => {
+      const w = section.querySelector('.o-big-title .o-big-title__inner');
+      const [x, xEnd] = (index % 2) ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+      gsap.fromTo(w, {  x  }, {
+        x: xEnd,
+        scrollTrigger: { 
+          trigger: '.ico_04', 
+          scrub: 0.5 
         }
-        if (velocity < 0) {
-          if (additionalXAnim) additionalXAnim.kill();
-          additionalX.val = -velocity / 4000;
-          additionalXAnim = gsap.to(additionalX, { val: 0 });
-        }
-      }
+      });
     });
-
 
 });
 // DOMContentLoaded  end
