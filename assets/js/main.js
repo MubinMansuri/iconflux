337280414aa031a10590471ade3e18f7947586e9
Usama Mansuri <mansuriusama786@gmail.com>
2023-05-29 15:32:58 +0530

    three new page added (angularJS, reactJS, VueJS ).

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,9 +158,9 @@ singeSlideSwiper3(".twoBoxCenter");
 
 var tl = gsap.timeline({});
 var rule = CSSRulePlugin.getRule(".flutter-banner:before"); //get the rule
-tl.fromTo(rule, { opacity:0, xPercent:0}, {opacity:1, xPercent:330, duration: 1 }),
-tl.fromTo(".flutter-banner #fade-in", {opacity: 0, y: 250}, {opacity: 1, duration: 1,  y: 0}),
-tl.fromTo(".flutter-banner .icon-wrp", {opacity: 1, scale: 0}, {opacity: 1, duration: 1,  scale: 1});
+tl.fromTo(rule, { opacity: 0, xPercent: '0px' }, { opacity: 1, xPercent: '330px', duration: 1 }),
+  tl.fromTo(".flutter-banner #fade-in", { opacity: 0, y: 50 }, { opacity: 1, duration: 1, y: 0 }),
+  tl.fromTo(".flutter-banner .icon-wrp", { opacity: 1, scale: 0 }, { opacity: 1, duration: 0.5, scale: 1 });
 
 // var rule = CSSRulePlugin.getRule(".flutter-banner:before"); //get the rule
 // gsap.to(rule, {opacity: 1, scale: 0}, {opacity: 1, duration: 1,  scale: 1});
